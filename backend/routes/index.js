a43633ea2c57f43578841d58978cf64b40452886
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-12-16 23:25:01 +0100

    feat: Implement authentication middleware for protected routes
    - Add authMiddleware to verify JWT token for protecting portfolio routes
    - Protect portfolio-related routes with authentication middleware
    - Update route handling for `/register` and `/login` to remain public

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import PortfolioController from '../controllers/PortfolioController';
 import AuthController from '../controllers/AuthController';
+import authMiddleware from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.get('/portfolios', PortfolioController.getAllPortfolios);
+// Public routes
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+
+
+// Protected routes
+router.use(authMiddleware); // Apply authMiddleware to protect the following routes
+
+router.get('/portfolios', PortfolioController.getAllPortfolios);
+router.get('/portfolios-prices', PortfolioController.getPortfolioWithPrices);
+router.post('/portfolios', PortfolioController.createPortfolio);
+router.put('/portfolios/:id', PortfolioController.updatePortfolio);
+router.delete('/portfolios/:id', PortfolioController.deletePortfolio);
 
 export default router;
