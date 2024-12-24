b229571318f59b7bd2cb40bd504190ccf73f1e12
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-12-24 14:24:33 +0100

    fix(authMiddleware): remove unnecessary return statements
    - Refactor authMiddleware to remove redundant return statements.
    - Clean up code for better readability and compliance with ESLint's "no-useless-return" rule.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,7 +9,6 @@ const router = express.Router();
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 
-
 // Protected routes
 router.use(authMiddleware); // Apply authMiddleware to protect the following routes
 
@@ -17,7 +16,7 @@ router.get('/portfolios', PortfolioController.getAllPortfolios);
 router.get('/portfolio/:portfolioId/prices', PortfolioController.getPortfolioWithPrices);
 router.post('/portfolios', PortfolioController.createPortfolio);
 router.put('/portfolios/:portfolioId', PortfolioController.updatePortfolio);
-router.delete('/portfolios/:id', PortfolioController.deletePortfolio);
+router.delete('/portfolio/:id', PortfolioController.deletePortfolio);
 router.delete('/user/:user_id', AuthController.deleteUser);
 router.put('/user/:user_id', AuthController.updateUser);
 
