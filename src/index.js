5dd369932d2301681676a3c2b4c2b2e971a0d213
emmashinko <emmanuelchris4603@gmail.com>
2025-01-08 14:02:22 +0100

    carousel added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import CryptoContext from './CryptoContext';
+import "react-alice-carousel/lib/alice-carousel.css";
 
 // Create a root element using the new React 18 API
 const root = ReactDOM.createRoot(document.getElementById('root'));
